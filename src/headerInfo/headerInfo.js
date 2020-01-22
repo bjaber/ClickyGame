b1eabf0c4cd353b24eefa54c216c0ebabd5ccba4
Basem Jaber <basemjaber32@gmail.com>
2020-01-21 18:20:53 -0700

    all the code I have for this one

---
+++ src/headerInfo/headerInfo.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './headerInfo.css';
+
+const Navbar = () =>{
+return(
+
+<header className="scoreboard">
+  <div className="row">
+<div className="col-md-7 col-left">
+  <h3> Click On the Pictures! Dont Hit the Same picture twice!</h3>
+</div>
+<div className="col-md-5 col-right">
+  <h3> Current Score:0</h3>
+
+</div>
+
+
+  </div>
+
+</header>
+
+
+
+)
+
+
+};
+
+export default Navbar;
